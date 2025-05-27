f4ab9a26bea9c032b5668cc1b69623a3bbc786c0
dogofsunrising <tengbenkouta@gmail.com>
2025-05-27 14:00:04 +0900

    実装

--- ap25unit1/myplayer/MyGame.java
+++ ap25unit1/myplayer/MyGame.java
@@ -7,7 +7,7 @@ import java.util.stream.*;
 
 public class MyGame {
   public static void main(String args[]) {
-    var player1 = new myplayer.MyPlayer(BLACK);
+    var player1 = new myplayer.HumanPlayer(BLACK);
     // Randomのプレイヤー設定（白色）
     var player2 = new myplayer.RandomPlayer(WHITE);
     var board = new MyBoard();
