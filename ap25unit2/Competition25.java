52570e36babba211cf1b9c86bc1474b97e81bda4
dogofsunrising <tengbenkouta@gmail.com>
2025-06-03 13:48:33 +0900

    軽くコメント

--- ap25unit2/Competition25.java
+++ ap25unit2/Competition25.java
@@ -10,8 +10,8 @@ class Competition25 {
     Function<Color, Player[]> builder = (Color color) -> {
       return new Player[] {
           new p25x00.OurPlayer(color),
-          new myplayer.MyPlayer(color),
-          new ap25.league.RandomPlayer(color),
+          // new myplayer.MyPlayer(color),
+          // new ap25.league.RandomPlayer(color),
           new ap25.league.RandomPlayer(color),
       };
     };
